ea92c259893234dd90265690ee0a819080e2ea1c
Saibato <saibato.naga@pm.me>
2019-11-05 14:21:48 +0100

    Use bin_len, len, tal_arrz(), u8 in base64
    Signed-off-by: Saibato <saibato.naga@pm.me>

--- common/base64.h
+++ common/base64.h
@@ -5,6 +5,6 @@
 #include <ccan/tal/tal.h>
 
 char *b64_encode(const tal_t *ctx, const u8 *data, size_t len);
-char *b64_decode(const tal_t *ctx, char *str, size_t len);
+u8 *b64_decode(const tal_t *ctx, const char *str, size_t len);
 
 #endif /* LIGHTNING_COMMON_BASE64_H */
